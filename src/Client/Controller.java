59ff22ff92d6b686b5721eda0dc9c72d5808e3ab
KubaDDD <facior0@o2.pl>
2017-12-10 16:56:04 +0100

    Scena hub na tablicach.

--- src/Client/Controller.java
+++ src/Client/Controller.java
@@ -2,7 +2,4 @@ package Client;
 
 public class Controller {
 
-
-
-
 }
--- src/Client/Main.java
+++ src/Client/Main.java
@@ -2,6 +2,7 @@ package Client;
 
 import Client.Network.Connection;
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Parent;
@@ -65,29 +66,17 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         window = primaryStage;
 
+
+        Label[] lGames = new Label[10];
+        Button[] bEnterGame = new Button[10];
+        for(int i=0; i<10; i++) {
+            lGames[i] = new Label("Gra: "+(i+1));
+            bEnterGame[i] = new Button("Dołącz do gry");
+        }
+
+
         /*
         //lGame1.setText("text");
-        Label lGame1 = new Label("Gra nr: 1\tAktywnych graczy: 4\t");
-        Label lGame2 = new Label("Gra nr: 2\tAktywnych graczy: 4\t");
-        Label lGame3 = new Label("Gra nr: 3\tAktywnych graczy: 4\t");
-        Label lGame4 = new Label("Gra nr: 4\tAktywnych graczy: 4\t");
-        Label lGame5 = new Label("Gra nr: 5\tAktywnych graczy: 4\t");
-        Label lGame6 = new Label("Gra nr: 6\tAktywnych graczy: 4\t");
-        Label lGame7 = new Label("Gra nr: 7\tAktywnych graczy: 4\t");
-        Label lGame8 = new Label("Gra nr: 8\tAktywnych graczy: 4\t");
-        Label lGame9 = new Label("Gra nr: 9\tAktywnych graczy: 4\t");
-        Label lGame10 = new Label("Gra nr:10\tAktywnych graczy: 4\t");
-
-        Button bGame1 = new Button("Dołącz");
-        Button bGame2 = new Button("Dołącz");
-        Button bGame3 = new Button("Dołącz");
-        Button bGame4 = new Button("Dołącz");
-        Button bGame5 = new Button("Dołącz");
-        Button bGame6 = new Button("Dołącz");
-        Button bGame7 = new Button("Dołącz");
-        Button bGame8 = new Button("Dołącz");
-        Button bGame9 = new Button("Dołącz");
-        Button bGame10 = new Button("Dołącz");
 
         //lambda expression
         bGame1.setOnAction(e -> System.out.println("Enter the game"));
@@ -102,7 +91,7 @@ public class Main extends Application {
         bGame10.setOnAction(e -> System.out.println("Enter the game"));
 
         //Layout
-        //instantiatig the GridPane class
+        //instantiatig the GridPane class*/
         GridPane gridPaneHubLayout = new GridPane();
         gridPaneHubLayout.setMinSize(300, 300);
         gridPaneHubLayout.setPadding(new Insets(10, 10, 10, 10));
@@ -113,35 +102,19 @@ public class Main extends Application {
         gridPaneHubLayout.setAlignment(Pos.TOP_LEFT);
 
         //Setting nodes
-        gridPaneHubLayout.add(lGame1, 0, 0);
-        gridPaneHubLayout.add(bGame1, 1, 0);
-        gridPaneHubLayout.add(lGame2, 0, 1);
-        gridPaneHubLayout.add(bGame2, 1, 1);
-        gridPaneHubLayout.add(lGame3, 0, 2);
-        gridPaneHubLayout.add(bGame3, 1, 2);
-        gridPaneHubLayout.add(lGame4, 0, 3);
-        gridPaneHubLayout.add(bGame4, 1, 3);
-        gridPaneHubLayout.add(lGame5, 0, 4);
-        gridPaneHubLayout.add(bGame5, 1, 4);
-        gridPaneHubLayout.add(lGame6, 0, 5);
-        gridPaneHubLayout.add(bGame6, 1, 5);
-        gridPaneHubLayout.add(lGame7, 0, 6);
-        gridPaneHubLayout.add(bGame7, 1, 6);
-        gridPaneHubLayout.add(lGame8, 0, 7);
-        gridPaneHubLayout.add(bGame8, 1, 7);
-        gridPaneHubLayout.add(lGame9, 0, 8);
-        gridPaneHubLayout.add(bGame9, 1, 8);
-        gridPaneHubLayout.add(lGame10, 0, 9);
-        gridPaneHubLayout.add(bGame10, 1, 9);
+        for(int i=0; i<10; i++) {
+            gridPaneHubLayout.add(lGames[i], 0, i);
+            gridPaneHubLayout.add(bEnterGame[i], 1, i);
+        }
 
         //Setting a scene obect;
-        hub=new Scene(gridPaneHubLayout);*/
+        hub=new Scene(gridPaneHubLayout);
 
 
         //Swithing the scene: window.setScene( nazwa_sceny );
         window.setTitle("Chinese Checkers");
-        primaryStage.setScene(new Scene(createContent()));
-        //window.setScene(hub);
+        //primaryStage.setScene(new Scene(createContent()));
+        window.setScene(hub);
         window.show();
 
     }
